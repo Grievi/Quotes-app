8fe15dfb5dfec387e7eddbbfaa61c32bd6c878e5
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-21 23:41:28 +0300

    add a for loop directive and ngif directive to quotes

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Quote} from './quote'
+
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,5 @@ import {Quote} from './quote'
 })
 export class AppComponent {
 
-  // quotes: any [];
-
-  // constructor(){
-  //   this.quotes = ['Bite what you can Chew','When the Deal is Too Good Think Twice','Haraka haraka haina Bahati']
-  // }
-  quotes:Quote[]=[
-    {id:1, name:'Bite what you can Chew'},
-    {id:2,name:'When the Deal is Too Good Think Twice'},
-    {id:3,name:'Haraka haraka haina Bahati'},
-    {id:4,name:'Mganga hajigangi'},
-    {id:5, name:'Dawa ya Deni ni Kulipa'}
-  ];
+  
 }
--- quotes/src/app/quote/quote.component.ts
+++ quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quote[]=[
+    {id:1, name:'Bite what you can Chew'},
+    {id:2,name:'When the Deal is Too Good Think Twice'},
+    {id:3,name:'Haraka haraka haina Bahati'},
+    {id:4,name:'Mganga hajigangi'},
+    {id:5, name:'Dawa ya Deni ni Kulipa'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
