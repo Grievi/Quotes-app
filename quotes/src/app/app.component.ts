c8358df714a2cd4345dcdb338b4a4b2afb6719db
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-21 23:24:01 +0300

    add more Quotes to our app

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // quote = 'Bite what You can Chew'
-  quotes: any [];
 
-  constructor(){
-    this.quotes = ['Bite what you can Chew','When the Deal is Too Good Think Twice','Haraka haraka haina Bahati']
-  }
+  // quotes: any [];
+
+  // constructor(){
+  //   this.quotes = ['Bite what you can Chew','When the Deal is Too Good Think Twice','Haraka haraka haina Bahati']
+  // }
+  quotes:Quote[]=[
+    {id:1, name:'Bite what you can Chew'},
+    {id:2,name:'When the Deal is Too Good Think Twice'},
+    {id:3,name:'Haraka haraka haina Bahati'},
+    {id:4,name:'Mganga hajigangi'},
+    {id:5, name:'Dawa ya Deni ni Kulipa'}
+  ];
 }
