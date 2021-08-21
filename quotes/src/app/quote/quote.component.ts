3d14283bb761f8f35d2f104fc7e682484b296582
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-21 23:58:29 +0300

    add Author details to the said Quote

--- quotes/src/app/quote/quote.component.ts
+++ quotes/src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    {id:1, name:'Bite what you can Chew'},
-    {id:2,name:'When the Deal is Too Good Think Twice'},
-    {id:3,name:'Haraka haraka haina Bahati'},
-    {id:4,name:'Mganga hajigangi'},
-    {id:5, name:'Dawa ya Deni ni Kulipa'}
+    {id:1, name:'Bite what you can Chew', author:'White.S Man'},
+    {id:2,name:'When the Deal is Too Good Think Twice',author:'Kenny Rodgers 1st'},
+    {id:3,name:'Haraka haraka haina Bahati',author:'Mhenga 001'},
+    {id:4,name:'Mganga hajigangi',author:'Wahenga '},
+    {id:5, name:'Dawa ya Deni ni Kulipa',author:'Okush Babaa '}
   ];
   constructor() { }
 
