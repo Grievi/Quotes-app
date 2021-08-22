375f12caa8135eebd1c135a0c657ff5cb2140872
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-22 20:02:28 +0300

    edit all components

--- quotes/src/app/quote.ts
+++ quotes/src/app/quote.ts
@@ -1,7 +1,6 @@
 export class Quote {
-
     showDescription: boolean;
-    constructor(public id:number,public name: any,public author:any){
-       this.showDescription=false 
+    constructor(public id: number,public name: string,public description: string){
+      this.showDescription=false;
     }
 }
--- quotes/src/app/quote/quote.component.ts
+++ quotes/src/app/quote/quote.component.ts
@@ -9,14 +9,21 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
   
   quotes:Quote[]=[
-    new Quote(1, 'Bite what you can Chew', 'White.S Man'),
-    new Quote(2,'When the Deal is Too Good Think Twice','Kenny Rodgers 1st'),
-    new Quote(3,'Haraka haraka haina Bahati','Mhenga 001'),
-    new Quote(4,'Mganga hajigangi','Wahenga '),
-    new Quote(5, 'Dawa ya Deni ni Kulipa','Okush Babaa ')
-  ];
-  toggleDetails(index:number){
-    this.quotes[index].showDescription=!this.quotes[index].showDescription;
+    new Quote(1, 'Haraka haraka haina baraka','Wahenga Wahenguzi'),
+   new Quote (2,'Pole pole ndio mwendo','Wagenge Wahenguzi'),
+    new Quote(3,'Zubaa zuba utpata mwana si wako',' Wahenguzi Wenyewe'),
+    new Quote(4,'Omba serikali Kazi','Msee wa Nduthi'),
+    new Quote(5,'Mambo ya Stima wachia serikali','Meme Lord'),
+   new Quote (6,'Nitakufinya','Wahengezoo')
+   ];
+
+   toggleDetails(index:number){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  completeQuote(isComplete:any, index:number){
+    if(isComplete){
+      this.quotes.splice(index,1)
+    }
   }
   constructor() { }
 
